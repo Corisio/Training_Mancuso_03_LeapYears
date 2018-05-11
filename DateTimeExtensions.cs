236cc264c9c2e381cd2095d59e14586f1cf47d89
Ernesto Cuevas Crespo <cerbuno@gmail.com>
2018-05-11 09:05:38 +0200

    IsLeapYearShould.BeTrueIfIsDivisibleByFourHundred green

--- DateTimeExtensions.cs
+++ DateTimeExtensions.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace SandroMancusoTraining_Project3
 {
-    public class Class1
+    public static class DateTimeExtensions
     {
+        public static bool IsLeapYear(this DateTime date)
+        {
+            return date.Year % 400 == 0;
+        }
     }
 }
---
+++ IsLeapYearShould.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SandroMancusoTraining_Project3
+{
+    [TestFixture]
+    public class IsLeapYearShould
+    {
+        [Test]
+        public void BeTrueIfIsDivisibleByFourHundred()
+        {
+            var date = new DateTime(2000, 1, 1);
+
+            Assert.IsTrue(date.IsLeapYear());
+        }
+    }
+}
